da63cb095926061f8f42296e7f351af80f3852a9
Voon Wong <voon.wong@smartbear.com>
2025-08-20 16:25:46 +1000

    chore: remove swagger-mock-validator

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,10 +5,7 @@ import yaml from "js-yaml";
 import { omit } from "lodash-es";
 import path from "path";
 import { expect, it } from "vitest";
-import { SwaggerMockValidatorFactory } from "@pactflow/swagger-mock-validator";
 import { Comparator } from "../../src/compare";
-
-const swaggerMockValidator = SwaggerMockValidatorFactory.create();
 
 const parse = (spec: string) => {
   try {
@@ -42,39 +39,11 @@ for (const entry of fs.readdirSync(FIXTURES)) {
     );
   };
 
-  const runSmv = async () => {
-    try {
-      const smv = await swaggerMockValidator.validate({
-        mockPathOrUrl: pactFile,
-        specPathOrUrl: oasFile,
-      });
-      const results = [...smv.errors, ...smv.warnings]
-        .sort((a, b) =>
-          a.mockDetails.location.localeCompare(
-            b.mockDetails.location,
-            undefined,
-            { numeric: true },
-          ),
-        )
-        .map((r) =>
-          omit(r, ["source", "mockDetails.mockFile", "specDetails.specFile"]),
-        );
-      await expect(JSON.stringify(results, null, 2) + "\n").toMatchFileSnapshot(
-        smvResultFile,
-      );
-    } catch (error) {
-      // Swagger Mock Validator crashes!
-      await expect(error.message + "\n").toMatchFileSnapshot(smvResultFile);
-    }
-  };
-
   if (entry.endsWith("skip")) {
     it.skip(entry.replace(/\.skip$/, ""));
   } else if (entry.endsWith("only")) {
     it.only(entry.replace(/\.only/, ""), runOpc);
-    it.only(`${entry.replace(/\.only/, "")} SMV`, runSmv, 300_000);
   } else {
     it(entry, runOpc);
-    it(`${entry} SMV`, runSmv, 300_000);
   }
 }

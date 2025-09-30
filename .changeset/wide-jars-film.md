4a2c74fe34a7f6b82bb07716f654e5a2a15e3a5c
Voon Wong <voon.wong@smartbear.com>
2025-09-30 10:55:06 +1000

    feat: flatten allOf to handle difference between JSON-Schema and OAD interpretations

---
+++ .changeset/wide-jars-film.md
@@ -0,0 +1,5 @@
+---
+"@pactflow/openapi-pact-comparator": minor
+---
+
+Mutate schema to properly support OAD allOf
--- src/transform/responseSchema.ts
+++ src/transform/responseSchema.ts
@@ -1,9 +1,5 @@
 import { SchemaObject } from "ajv";
-import { each, get } from "lodash-es";
-import {
-  splitPath,
-  traverseWithDereferencing as traverse,
-} from "#utils/schema";
+import { traverseWithDereferencing as traverse } from "#utils/schema";
 
 export const transformResponseSchema = (
   schema: SchemaObject,
@@ -28,34 +24,11 @@ export const transformResponseSchema = (
 
   // a consumer may only use a subset of the provider *response* any field
   // marked as required in OAS, should be considered optional for pact testing
-
   traverse(schema, (s) => {
     if (s.oneOf) {
       return; // discriminator is required to be a valid schema
     }
     delete s.required;
-  });
-
-  // OpenAPI defines allOf to mean the union of all sub-schemas. JSON-Schema
-  // defines allOf to mean that *every* sub-schema needs to be satisfied. In
-  // draft 2019-09, JSON-Schema added "unevaluatedProperties" to support this
-  // behaviour
-  traverse(schema, (s) => {
-    if (s.allOf) {
-      each(s.allOf, (ss) => {
-        let subschema = ss;
-        if (subschema.$ref) {
-          subschema = get(schema, splitPath(subschema.$ref));
-        }
-        delete subschema.additionalProperties;
-        if (subschema.allOf) {
-          // traversal is depth-first; if nested allOf, remove
-          // unevaluatedProperties from previously set deeper schema
-          delete subschema.unevaluatedProperties;
-        }
-      });
-      s.unevaluatedProperties = false;
-    }
   });
 
   return schema;

5e37e2f583251e1b909278623ea6e35909cec93e
Voon Wong <voon.wong@smartbear.com>
2026-01-14 12:50:40 +1100

    chore: fix bin path

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,20 @@ import json from "@rollup/plugin-json";
 
 const plugins = [commonjs(), typescript(), nodeResolve(), json()];
 
+const onwarn = (warning, warn) => {
+  if (
+    warning.code === "CIRCULAR_DEPENDENCY" &&
+    warning.message.includes("node_modules")
+  ) {
+    return;
+  }
+  warn(warning);
+};
+
 export default [
   {
     input: "src/cli.ts",
+    onwarn,
     output: {
       file: "dist/cli.cjs",
       format: "cjs",
@@ -17,6 +28,7 @@ export default [
   },
   {
     input: "src/index.ts",
+    onwarn,
     output: {
       file: "dist/index.mjs",
       format: "es",
@@ -26,6 +38,7 @@ export default [
   },
   {
     input: "src/index.ts",
+    onwarn,
     output: {
       file: "dist/index.cjs",
       format: "cjs",
